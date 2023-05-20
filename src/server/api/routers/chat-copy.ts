84248e1687b441166b5bd2b138269c618534998f
Saad Ahmed <saadahmednayerazem@gmail.com>
2023-05-20 19:26:37 +0500

    Error context_length_exceeded fixed

---
+++ src/server/api/routers/chat-copy.ts
@@ -0,0 +1,103 @@
+import { z } from "zod";
+import { WeaviateStore } from "langchain/vectorstores/weaviate";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ConversationalRetrievalQAChain,loadQARefineChain } from "langchain/chains";
+import weaviate from "weaviate-ts-client";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+import { env } from "~/env.mjs";
+
+export const wvClient = weaviate.client({
+  scheme: "https",
+  host: env.WEAVIATE_HOST,
+  apiKey: new weaviate.ApiKey(env.WEAVIATE_API_KEY),
+});
+
+const QAPromptTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+Helpful Answer:`;
+
+const QuestionGeneratorTemplate = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const model = new ChatOpenAI({ openAIApiKey: env.OPENAI_API_KEY, temperature: 0, modelName: "gpt-3.5-turbo" });
+const embeddings = new OpenAIEmbeddings({ openAIApiKey: env.OPENAI_API_KEY });
+
+export const chatRouter = createTRPCRouter({
+  prompt: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        question: z.string(),
+        qaPromptTemplate: z.string(),
+        questionGeneratorTemplate: z.string(),
+        history: z.array(z.object({ agent: z.string(), text: z.string() })),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { userId, question, qaPromptTemplate, questionGeneratorTemplate, history } = input;
+
+      try {
+        const vectorStore = await WeaviateStore.fromExistingIndex(embeddings, {
+          client: wvClient,
+          indexName: "Documents",
+          metadataKeys: ["userId"],
+        });
+
+        console.log("question:"+question);
+        const chatHistory = history.map((message) => message.text);
+        console.log({ chatHistory });
+
+        const chain = ConversationalRetrievalQAChain.fromLLM(
+          model,
+          vectorStore.asRetriever(undefined, {
+            distance: 0,
+            where: {
+              path: ["userId"],
+              operator: "Equal",
+              valueText: userId,
+            },
+          }),
+          {
+            qaTemplate: QAPromptTemplate,
+            questionGeneratorTemplate: QuestionGeneratorTemplate,
+            returnSourceDocuments: true, //The number of source documents returned is 4 by default
+          },
+        );
+        // const chain = new ConversationalRetrievalQAChain({
+        //   combineDocumentsChain: loadQARefineChain(model),
+        //   retriever: vectorStore.asRetriever(undefined, {
+        //     distance: 0,
+        //     where: {
+        //       path: ["userId"],
+        //       operator: "Equal",
+        //       valueText: userId,
+        //     },
+        //   })
+        // });
+        // const res = await chain.call({ 
+        //   question: question,
+        //   chat_history: chatHistory,
+        // });
+        // console.log({ res });
+
+        // const response = res.output_text as string;
+        // return response;
+      } catch (e) {
+        console.error(e);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+        });
+      }
+    }),
+});
