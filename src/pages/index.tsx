3d0221d54989fc3778e1f79beb37473188433858
Saad Ahmed <saadahmednayerazem@gmail.com>
2023-08-31 10:21:37 +0500

    demo to client Brian Kwizera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,29 +16,34 @@ const Home: NextPage = () => {
         <meta name="description" content="Saad Ahmed " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main>
-        <script src="http://tryauthor.code/public/iframeResizer.contentWindow.min.js" async />
-        <div className="row m-0 p-0">
-          <div className="col-md-4 sidebar">
-            <div className="card border-0 shadow-sm">
-              <div className="card-body card-body-padding">
-                <h6 className="mt-1 ml-3 fs-16 fw-bolder">Chat With (pdf, docx, csv, txt, json) documents using ChatGPT 4</h6>
-                <p className="fs-12 text-muted mb-4 small">Chat With (pdf, docx, csv, txt, json) documents using ChatGPT 4</p>
+      <main className="bg-image">
 
-                <FileUpload />
+        {/*<script src="http://tryauthor.code/public/iframeResizer.contentWindow.min.js" async />*/}
+        <div className="container mt-5">
 
-                <FileList />
+          <div className="row">
+            <div className="col-md-4 sidebar">
+              <div className="card shadow border-left-primary">
+                <div className="card-body card-body-padding">
+                  <h6 className="mt-1 ml-3 fs-16 fw-bolder">Chat With (pdf, docx, csv, txt, json) Documents Using ChatGPT 4</h6>
+                  <p className="fs-12 text-muted mb-4 small">Chat With (pdf, docx, csv, txt, json) Documents Using ChatGPT 4</p>
 
-                {/*<Text size="lg" className="mt-5"><strong>Db-Id={userId}</strong></Text>*/}
-                {/*<EditPrompts />*/}
+                  <FileUpload />
+
+                  <FileList />
+
+                  {/*<Text size="lg" className="mt-5"><strong>Db-Id={userId}</strong></Text>*/}
+                  {/*<EditPrompts />*/}
+                </div>
+              </div>
+            </div>
+            <div className="col-md-8">
+              <div className="card card-body card-body-padding card-panel shadow bg-white border-left-primary">
+                <Chat />
               </div>
             </div>
           </div>
-          <div className="col-md-8">
-            <div className="card card-body card-body-padding card-panel shadow-sm border-0 bg-white">
-              <Chat />
-            </div>
-          </div>
+
         </div>
       </main>
     </>
